c49d6cc56950d1e49d8d16ff23c842b6fe92a348
performer <15815094902@163.com>
2020-10-08 18:27:32 +0800

    '展示index页面，创建utlis工具包'

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from models import db
 from views import index_blu
+from utils.common import show_top_6_news_style
 
 
 # 创建flask应用对象
@@ -17,6 +18,9 @@ app.register_blueprint(index_blu)
 
 # 初始化数据库
 db.init_app(app)
+
+# 添加过滤器
+app.add_template_filter(show_top_6_news_style)
 
 # 添加数据库迁移等工具
 manager = Manager(app)
--- views/index.py
+++ views/index.py
@@ -4,4 +4,4 @@ from flask import render_template
 
 @index_blu.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index/index.html")
